874f50b4f13e2ff2d118ae784634d1ea7b2fc5c9
salmandma <salmandwimaulanaakbar96@gmail.com>
2023-07-11 17:32:42 +0700

    feat: :sparkles: add new feature crud

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import 'react-toastify/dist/ReactToastify.css';
+import Product from './components/products/products';
+import { Provider } from 'react-redux';
+import store from './store/store';
+import Home from './components/Home';
 
-const router = createBrowserRouter([]);
+const router = createBrowserRouter([
+ {
+  path: '/',
+  element: <Product />,
+ },
+ {
+  path: '/home',
+  element: <Home />,
+ },
+]);
 
 function App() {
- return <RouterProvider router={router} />;
+ return (
+  <Provider store={store}>
+   <RouterProvider router={router} />
+  </Provider>
+ );
 }
 
 export default App;
